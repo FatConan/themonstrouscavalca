e1f07bb913b9c2d2d763f8888cadf8273d44fe29
Ian Usher <ian@codeplay.com>
2018-09-10 22:03:09 +0100

    Updating 404 pages and correcting footer

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,9 +1,7 @@
-console.log("Executing MAIN.js");
 requirejs(["build"], function(){
     'use strict';
     requirejs(["jquery", "domReady"], function ($, domReady) {
         domReady(function(){
-           console.log("DOMEREADY!");
         });
     });
 });
