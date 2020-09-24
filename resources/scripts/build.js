e4804456f184da76ccee42572fd6bf0662cbf639
Ian Usher <iancapacitated@gmail.com>
2020-09-24 11:03:33 +0100

    Adding updated jquery

--- resources/scripts/build.js
+++ resources/scripts/build.js
@@ -4,7 +4,7 @@ requirejs.config({
     paths: {
         'text': ['https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min'],
         'underscore': ['https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min'],
-        'jquery': ['https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min'],
+        'jquery': ['https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min'],
         'jquery-ui': ['https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min'],
         'domReady': ['./common/DomReady']
     },
